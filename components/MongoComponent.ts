31574e9049daef52aac317d8d271368f56ef2b47
myungki cho <myungkicho@myungkiui-MacBookAir.local>
2022-07-22 22:34:46 +0900

    Create redis component

--- components/MongoComponent.ts
+++ components/MongoComponent.ts
@@ -4,24 +4,25 @@ import { KeyPair, SecurityGroup } from "@pulumi/aws/ec2";
 import { SecurityGroupFactory } from "../services/security/SecurityGroupFactory";
 import { MongoSecurityGroup } from "../services/security/MongoSecurityGroup";
 
+export const START_MONGO_SERVICE = `
+        #!bin/bash
+        sudo service mongod start
+        `
 export const MONGO_COMPONENT = "pkg:index:Mongocomponent";
 
 export class MongoComponent extends pulumi.ComponentResource {
     constructor(name: string, opts: any) {
         super(MONGO_COMPONENT, name);
         const stackName = pulumi.getStack();
-        const securtiyGroup: SecurityGroup = SecurityGroupFactory.getSecurityGroup(MongoSecurityGroup);
-        const userData = `
-        #!bin/bash
-        sudo service mongod start
-        `
+        const securityGroup: SecurityGroup = SecurityGroupFactory.getSecurityGroup(MongoSecurityGroup);
+        const userData = START_MONGO_SERVICE
         const instance = new aws.ec2.Instance(`chord-play-mongo-${stackName}`,{
             tags: {
-                Name: "chord-play-mongo-db-2"
+                Name: `chord-play-mongo-db-${stackName}`
             },
             instanceType: "t2.small",
             ami: "ami-07405dd2e31f0683a",
-            vpcSecurityGroupIds: [securtiyGroup.id],
+            vpcSecurityGroupIds: [securityGroup.id],
             userData: userData
         }, opts);
     }
--- components/RedisComponent.ts
+++ components/RedisComponent.ts
@@ -1,11 +1,30 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import { SecurityGroupFactory } from "../services/security/SecurityGroupFactory";
+import { RedisSecurityGroup } from "../services/security/RedisSecurityGroup";
+export const START_REDIS_SERVICE = `
+    #!bin/bash
+    sudo service mongod start
+`
 export const REDIS_COMPONENT = "pkg:index:Rediscomponent"
+
 export class RedisComponent extends pulumi.ComponentResource {
     constructor(name: string, opts: any) {
         super(REDIS_COMPONENT, name);
         const stackName = pulumi.getStack();
+        const securityGroup = SecurityGroupFactory.getSecurityGroup(RedisSecurityGroup);
+        const userData = START_REDIS_SERVICE
+        
+        const instance = new aws.ec2.Instance(`chord-play-progress-redis-${stackName}`,{
+            tags: {
+                Name: `chord-play-progress-redis-${stackName}`
+            },
+            instanceType: "t2.small",
+            ami: "ami-09884b1892882c41d",
+            vpcSecurityGroupIds: [securityGroup.id],
+            userData: userData
+        }, opts);
+
     }
 
 }
