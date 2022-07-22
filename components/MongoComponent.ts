3a420a22c5ee59d2d4a08150148e1b775edee2c1
myungki cho <myungkicho@myungkiui-MacBookAir.local>
2022-07-22 17:01:40 +0900

    ProgressComponent

--- components/MongoComponent.ts
+++ components/MongoComponent.ts
@@ -1,14 +1,14 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import { KeyPair, SecurityGroup } from "@pulumi/aws/ec2";
-import { InstanceArgs } from "@pulumi/aws/ec2";
 import { SecurityGroupFactory } from "../services/security/SecurityGroupFactory";
 import { MongoSecurityGroup } from "../services/security/MongoSecurityGroup";
-import { CustomSecurityGroup } from "../services/security/CustomSecurityGroup";
+
+export const MONGO_COMPONENT = "pkg:index:Mongocomponent";
 
 export class MongoComponent extends pulumi.ComponentResource {
     constructor(name: string, opts: any) {
-        super("pkg:index:MongoComponent", name);
+        super(MONGO_COMPONENT, name);
         const stackName = pulumi.getStack();
         const securtiyGroup: SecurityGroup = SecurityGroupFactory.getSecurityGroup(MongoSecurityGroup);
         
@@ -16,7 +16,7 @@ export class MongoComponent extends pulumi.ComponentResource {
         !bin/bash
         sudo service mongod start
         `
-        
+
         const instance = new aws.ec2.Instance(`chord-play-mongo-${stackName}`,{
             instanceType: "t2.small",
             ami: "chord-play-mongo",
--- components/ProgressComponent.ts
+++ components/ProgressComponent.ts
@@ -1,2 +1,11 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
+
+export const PROGRESS_COMPONENT = "pkg:index:ProgressComponent";
+
+export class ProgressComponent extends pulumi.ComponentResource {
+    constructor(name, opts) {
+        super(name,opts);
+
+    }
+}
